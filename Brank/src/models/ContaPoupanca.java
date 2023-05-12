157084b63f4920671a1777411bac6ded58e82420
Winchester890 <126791337+Winchester890@users.noreply.github.com>
2023-05-12 00:49:31 -0300

    Update ContaPoupanca.java

--- Brank/src/models/ContaPoupanca.java
+++ Brank/src/models/ContaPoupanca.java
@@ -1,10 +1,10 @@
 package models;
 
-public class ContaPoupanca extends Conta {
+public class ContaPoupanca extends Conta implements Tributavel {
     private int diaAniversario;
     private double juros;
 
-    public ContaPoupanca(int numero, int agencia, String banco, double saldo, int diaAniversario, double juros) {
+    public ContaPoupanca(String numero, int agencia, String banco, double saldo, int diaAniversario, double juros) {
         super(numero, agencia, banco, saldo);
         this.diaAniversario = diaAniversario;
         this.juros = juros;
@@ -13,5 +13,10 @@ public class ContaPoupanca extends Conta {
     public double getSaldo() {
         return this.saldo + this.juros * this.saldo;
 
+    }
+
+    @Override
+    public double getImposto() {
+        return 100 + 100 * juros;
     }
 }
