7115e8eac1fd9c5c5061bc1c21ae151fb421dbf6
Winchester890 <126791337+Winchester890@users.noreply.github.com>
2023-05-12 00:49:48 -0300

    Update ContaSalario.java

--- Brank/src/models/ContaSalario.java
+++ Brank/src/models/ContaSalario.java
@@ -1,13 +1,18 @@
 package models;
 
-public class ContaSalario extends Conta {
+public class ContaSalario extends Conta implements Tributavel {
     private double salario;
 
-    public ContaSalario(int numero, int agencia, String banco, double saldo, double salario) {
+    public ContaSalario(String numero, int agencia, String banco, double saldo, double salario) {
         super(numero, agencia, banco, saldo);
         this.salario = salario;
     }
     public double getSaldo() {
         return this.saldo + this.salario;
+    }
+
+    @Override
+    public double getImposto() {
+        return 100 + (salario * 0.01);
     }
 }
